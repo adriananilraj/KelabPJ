26117ff1de2d38f67d1bc2dbbc4709ab1f340fa5
adriananilraj <anilraja33@gmail.com>
2023-06-07 21:46:53 +0800

    testing changes

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ currentYearElement.textContent = currentYear;
 // Navbar Container 
 var Navbar = document.getElementById('NavbarContainer');
 var NavbarWeb = document.getElementById('navbarWeb');
-
+var screenWidth = window.innerWidth;
 window.addEventListener('resize', function() {
-    var screenWidth = window.innerWidth;
+    
     if (screenWidth <= 770) {
         NavbarWeb.style.display = 'none';
         Navbar.style.display = 'block';
@@ -33,5 +33,18 @@ window.addEventListener('resize', function() {
         NavbarWeb.style.display = 'block'
         Navbar.style.display = 'none';
     }
-  });
-  
+});
+
+
+(function() {
+    // Handle If Load Responsive
+    console.log('work')
+    if (screenWidth <= 770) {
+        NavbarWeb.style.display = 'none';
+        Navbar.style.display = 'block';
+    } else {
+        NavbarWeb.style.display = 'block'
+        Navbar.style.display = 'none';
+    }
+})();
+
